d89208593daa77bceb224206cc5681e0c10d0aaf
andres alcocer <andresalcocer7@yahoo.com>
2020-10-02 13:03:50 -0400

    created oder, user, and product schemas

--- server/config/db.js
+++ server/config/db.js
@@ -7,9 +7,9 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useCreateIndex: true
     })
-    console.log(`MongoDB Connected: ${connection.host}`)
+    console.log(`MongoDB Connected: ${connection.connection.host}`.cyan.underline)
   } catch (error) {
-    console.log(`Error ${error}`)
+    console.log(`Error ${error}`.red.underline.bold)
     process.exit(1)
   }
 }
