51a3103b22878126f68c7d558225cd2391a2c7ce
andres alcocer <andresalcocer7@yahoo.com>
2020-10-06 15:54:37 -0400

    implemented json web tokens

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,8 @@
 import User from '../models/userModel.js'
 import asyncHandler from 'express-async-handler'
 
+import generateToken from '../utils/generateToken.js'
+
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body
   const user = await User.findOne({ email })
@@ -15,7 +17,7 @@ const authUser = asyncHandler(async (req, res) => {
     name: user.name,
     email: user.email,
     isAdmin: user.isAdmin,
-    token: null
+    token: generateToken(user._id)
   })
 })
 
