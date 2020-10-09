1540b2d4ba9cfe98e07575ec59a38b310c796c9a
andres alcocer <andresalcocer7@yahoo.com>
2020-10-08 22:10:05 -0400

    created RegisterScreen component

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import HomeScreen from './screens/HomeScreen'
 import ProductDetailScreen from './screens/ProductDetailScreen'
 import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Container>
           <Route path='/' component={HomeScreen} exact />
           <Route path='/login' component={LoginScreen} />
+          <Route path='/register' component={RegisterScreen} />
           <Route path='/product/:id' component={ProductDetailScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
         </Container>
