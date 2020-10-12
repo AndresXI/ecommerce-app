f589dbf60a7e69982e1d70199bbaba6b5ef3222a
andres alcocer <andresalcocer7@yahoo.com>
2020-10-11 20:27:11 -0400

    now able to update user profile details

--- client/src/screens/ProfileScreen.js
+++ client/src/screens/ProfileScreen.js
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux'
 
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { getUserDetails } from '../actions/userActions'
+import { getUserDetails, updateUserProfile } from '../actions/userActions'
 
 const ProfileScreen = ({ location, history }) => {
   const [email, setEmail] = useState('')
@@ -19,6 +19,9 @@ const ProfileScreen = ({ location, history }) => {
 
   const userLogin = useSelector((state) => state.userLogin)
   const { userInfo } = userLogin
+
+  const userUpdateProfile = useSelector((state) => state.userUpdateProfile)
+  const { success } = userUpdateProfile
 
   useEffect(() => {
     if (!userInfo) {
@@ -39,7 +42,7 @@ const ProfileScreen = ({ location, history }) => {
     if (password !== confirmPassword) {
       setMessage('Passwords do not match')
     } else {
-      // dispatch action
+      dispatch(updateUserProfile({ id: user._id, name, email, password }))
     }
   }
 
@@ -48,7 +51,7 @@ const ProfileScreen = ({ location, history }) => {
       <Col md='3'>
         <h2>User Profile</h2>
         {error && <Message variant='danger'>{error.message}</Message>}
-        {message && <Message variant='danger'>{message}</Message>}
+        {success && <Message variant='success'>Profile Updated</Message>}
         {loading && <Loader />}
         <Form onSubmit={submitHandler}>
           <Form.Group controlId='name'>
