3bf67582d40afdc1762dfafe4264bea07c70b961
andres alcocer <andresalcocer7@yahoo.com>
2020-10-12 18:54:23 -0400

    created shipping address action and reducer

--- client/src/actions/cartActions.js
+++ client/src/actions/cartActions.js
@@ -1,5 +1,9 @@
 import axios from 'axios'
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING_ADDRESS,
+} from '../constants/cartConstants'
 
 export const addToCart = (productId, quantity) => async (
   dispatch,
@@ -29,4 +33,13 @@ export const removeFromCart = (id) => (dispatch, getState) => {
   })
 
   localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+}
+
+export const saveShippingAddress = (data) => (dispatch) => {
+  dispatch({
+    type: CART_SAVE_SHIPPING_ADDRESS,
+    payload: data,
+  })
+
+  localStorage.setItem('shippingAddress', JSON.stringify(data))
 }
