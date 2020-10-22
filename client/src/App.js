7b82839c01b6091a439700d6dd3b5b070c0b6632
andres alcocer <andresalcocer7@yahoo.com>
2020-10-22 17:44:39 -0400

    fixed user info in orderController

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
+import OrderScreen from './screens/OrderScreen'
 
 const App = () => {
   return (
@@ -22,6 +23,7 @@ const App = () => {
         <Container>
           <Route path='/' component={HomeScreen} exact />
           <Route path='/login' component={LoginScreen} />
+          <Route path='/order/:id' component={OrderScreen} />
           <Route path='/register' component={RegisterScreen} />
           <Route path='/shipping' component={ShippingScreen} />
           <Route path='/payment' component={PaymentScreen} />
