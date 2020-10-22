a4a5fbcd1d884d2d5b31ffbe386532609d9119db
andres alcocer <andresalcocer7@yahoo.com>
2020-10-22 18:07:17 -0400

    created updateOrderToPaid api endpoint

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -44,4 +44,24 @@ const getOrderById = asyncHandler(async (req, res) => {
   res.status(201).json(order)
 })
 
-export { addOrderItems, getOrderById }
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id)
+
+  if (!order) {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+
+  order.isPaid = true
+  order.paidAt = Date.now()
+  order.paymentResult = {
+    id: req.body.id,
+    status: req.body.status,
+    update_time: req.body.update_time,
+    email_address: req.body.payer.email_address,
+  }
+  const updatedOrder = await order.save()
+  res.status(201).json(updatedOrder)
+})
+
+export { addOrderItems, getOrderById, updateOrderToPaid }
