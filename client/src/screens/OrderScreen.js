ee27ece5725dd3f706695b231f0b9e9419d90619
andres alcocer <andresalcocer7@yahoo.com>
2020-10-28 12:32:25 -0400

    implemented paypal script

--- client/src/screens/OrderScreen.js
+++ client/src/screens/OrderScreen.js
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Row, Col, ListGroup, Image, Card } from 'react-bootstrap'
+import axios from 'axios'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
@@ -8,18 +9,41 @@ import Loader from '../components/Loader'
 import { getOrderDetails } from '../actions/orderActions'
 
 const OrderScreen = ({ match }) => {
+  const [sdkReady, setSdkReady] = useState(false)
+
   const orderId = match.params.id
   const dispatch = useDispatch()
 
   const orderDetails = useSelector((state) => state.orderDetails)
   const { order, loading, error } = orderDetails
 
+  const orderPay = useSelector((state) => state.orderPay)
+  const { loading: loadingPay, success: successPay } = orderPay
+
   useEffect(() => {
-    // fetch most recent order
-    if (!order || order._id !== orderId) {
-      dispatch(getOrderDetails(orderId))
+    const addPayPalScript = async () => {
+      const { data: clientId } = await axios.get('/api/config/paypal')
+      const script = document.createElement('script')
+      script.type = 'text/javascript'
+      script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}`
+      script.async = true
+      script.onload = () => {
+        setSdkReady(true)
+      }
+      document.body.appendChild(script)
     }
-  }, [order, orderId, dispatch])
+
+    // fetch most recent order
+    if (!order || order._id !== orderId || successPay) {
+      dispatch(getOrderDetails(orderId))
+    } else if (!order.isPaid) {
+      if (!window.paypal) {
+        addPayPalScript()
+      } else {
+        setSdkReady(true)
+      }
+    }
+  }, [order, orderId, dispatch, successPay])
 
   return loading ? (
     <Loader />
