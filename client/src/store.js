507d7cf2cc35728f246a4ccfe73c7bd6a59b47aa
andres alcocer <andresalcocer7@yahoo.com>
2020-10-31 22:05:36 -0400

    implemented udpate user functionality

--- client/src/store.js
+++ client/src/store.js
@@ -14,6 +14,7 @@ import {
   userUpdateProfileReducer,
   userListReducer,
   userDeleteReducer,
+  userUpdateReducer,
 } from './reducers/userReducers'
 import {
   orderCreateReducer,
@@ -31,6 +32,7 @@ const reducer = combineReducers({
   userDetails: userDetailsReducer,
   userList: userListReducer,
   userUpdateProfile: userUpdateProfileReducer,
+  userUpdate: userUpdateReducer,
   userDelete: userDeleteReducer,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
