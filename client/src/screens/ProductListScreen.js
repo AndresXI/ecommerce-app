129943769f0f2554f6eeb81a583039489b2e9369
andres alcocer <andresalcocer7@yahoo.com>
2020-11-02 14:43:16 -0500

    implemented admin create product functionality

--- client/src/screens/ProductListScreen.js
+++ client/src/screens/ProductListScreen.js
@@ -5,7 +5,12 @@ import { useDispatch, useSelector } from 'react-redux'
 
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { listProducts, deleteProduct } from '../actions/productActions'
+import {
+  listProducts,
+  deleteProduct,
+  createProduct,
+} from '../actions/productActions'
+import { PRODUCT_CREATE_RESET } from '../constants/productConstants'
 
 const ProductListScreen = ({ history, match }) => {
   const dispatch = useDispatch()
@@ -20,22 +25,43 @@ const ProductListScreen = ({ history, match }) => {
     success: successDelete,
   } = productDelete
 
+  const productCreate = useSelector((state) => state.productCreate)
+  const {
+    loading: loadingCreate,
+    error: errorCreate,
+    success: successCreate,
+    product: createdProduct,
+  } = productCreate
+
   const userLogin = useSelector((state) => state.userLogin)
   const { userInfo } = userLogin
 
   useEffect(() => {
-    if (!(userInfo && userInfo.isAdmin)) {
+    dispatch({ type: PRODUCT_CREATE_RESET })
+
+    if (!userInfo.isAdmin) {
       history.push('/login')
     }
-    dispatch(listProducts())
-  }, [dispatch, history, userInfo, successDelete])
+    if (!successCreate) {
+      dispatch(listProducts())
+    } else {
+      history.push(`/admin/product/${createdProduct._id}/edit`)
+    }
+  }, [
+    dispatch,
+    history,
+    userInfo,
+    successDelete,
+    successCreate,
+    createdProduct,
+  ])
 
   const deleteHandler = (productId) => {
     dispatch(deleteProduct(productId))
   }
 
-  const createProductHandler = (product) => {
-    console.log('crate product hancler')
+  const createProductHandler = () => {
+    dispatch(createProduct())
   }
 
   return (
@@ -52,6 +78,8 @@ const ProductListScreen = ({ history, match }) => {
       </Row>
       {loadingDelete && <Loader />}
       {errorDelete && <Message variant='danger'>{errorDelete.message}</Message>}
+      {loadingCreate && <Loader />}
+      {errorCreate && <Message variant='danger'>{errorCreate.message}</Message>}
       {loading ? (
         <Loader />
       ) : error ? (
